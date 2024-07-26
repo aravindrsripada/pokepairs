9c8ded9027a5a3a79647ca99005e71cf1caa3d55
aravindrs44 <aravindrs@hotmail.com>
2024-07-26 01:08:31 -0700

    init

---
+++ src/utils/tiles.tsx
@@ -0,0 +1,10 @@
+export type Mon = 'Pikachu' | 'Charizard' | 'Gengar' | 'Lucario' | 'Snorlax' | 'Espeon' | 'Blaziken' | 'Milotic' | 'Gardevoir' | 'Salamence' | 'Metagross' | 'Jolteon' | 'Vaporeon' | 'Sylveon' | 'Infernape' | 'Sceptile' | 'Swampert' | 'Torterra' | 'Empoleon' | 'Luxray' | 'Togekiss' | 'Leafeon' | 'Glaceon' | 'Garchomp' | 'Talonflame' | 'Greninja';
+
+export type Tile = {
+    mon: Mon,
+    number: number
+}
+
+
+export const mons: Mon[] = [
+    "Pikachu", "Charizard", "Gengar", "Lucario", "Snorlax", "Espeon", "Blaziken", "Milotic", "Gardevoir", "Salamence", "Metagross", "Jolteon", "Vaporeon", "Sylveon", "Infernape", "Sceptile", "Swampert", "Torterra", "Empoleon", "Luxray", "Togekiss", "Leafeon", "Glaceon", "Garchomp", "Talonflame", "Greninja"]
